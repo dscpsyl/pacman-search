34a7053203f0d98aa8c6eeb312a070dcc82437ab
gento <41165204+dscpsyl@users.noreply.github.com>
2024-03-03 00:46:26 -0800

    refactor:Move tree creation into base class

--- multiAgents.py
+++ multiAgents.py
@@ -104,31 +104,8 @@ class MultiAgentSearchAgent(Agent):
         self.index = 0 # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
-
-class MinimaxAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent
-    """
-
-    def getAction(self, gameState):
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction.
-
-        Here are some method calls that might be useful when implementing minimax.
-
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
-
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
-
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
-        """
-        "*** YOUR CODE HERE ***"
-        
+    
+    def _populateTree(self, gameState):
         # Check the depth
         if self.depth == 0:
             raise Exception("MinimaxAgent::getAction - Depth is 0")
@@ -160,7 +137,34 @@ class MinimaxAgent(MultiAgentSearchAgent):
             currentDepth+= 1
             currentAgentIndex = (currentAgentIndex + 1) % gameState.getNumAgents()
         
-        # We know that we have reached the leaf nodes of our tree    
+        return tree
+
+class MinimaxAgent(MultiAgentSearchAgent):
+    """
+    Your minimax agent
+    """
+
+    def getAction(self, gameState):
+        """
+        Returns the minimax action from the current gameState using self.depth
+        and self.evaluationFunction.
+
+        Here are some method calls that might be useful when implementing minimax.
+
+        gameState.getLegalActions(agentIndex):
+        Returns a list of legal actions for an agent
+        agentIndex=0 means Pacman, ghosts are >= 1
+
+        gameState.generateSuccessor(agentIndex, action):
+        Returns the successor game state after an agent takes an action
+
+        gameState.getNumAgents():
+        Returns the total number of agents in the game
+        """
+        "*** YOUR CODE HERE ***"
+        
+        
+        tree = self._populateTree(gameState) 
         
         # Get the leaves of the tree
         leafs = tree.leafs()
